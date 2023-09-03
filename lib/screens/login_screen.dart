06759be9d2cc6d260ef260f76b3e3e3713664bfb
Valery Vallejo <valery.antonella19@gmail.com>
2023-09-02 19:29:04 -0500

    Login Redesign

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -15,44 +15,49 @@ class LoginScreen extends StatelessWidget {
       appBar: null,
       body: SingleChildScrollView(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Wrap(
-              children: [Container(
-                  width: double.infinity,
-                  color: const Color(0xFFF91659),
-                  child: Padding(
-                    padding: const EdgeInsets.only(top: 30),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        const Text(
-                          'PoliMatch',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 48.0,
-                            color: Colors.white,
-                          ),
+              children: [
+                Container(
+                    width: double.infinity,
+                    color: const Color(0xFFF91659),
+                    child: Padding(
+                      padding: const EdgeInsets.only(top: 30),
+                      child: Padding(
+                        padding: const EdgeInsets.only(top: 35.0, bottom: 35.0),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            const Text(
+                              'PoliMatch',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 48.0,
+                                color: Colors.white,
+                              ),
+                            ),
+                            Image.asset(
+                              'lib/images/logoLogin.png', // Ruta de la imagen del logotipo
+                              width: 200.0, // Ancho de la imagen
+                              height: 200.0, // Alto de la imagen
+                            ),
+                          ],
                         ),
-                        Image.asset(
-                          'lib/images/logoLogin.png', // Ruta de la imagen del logotipo
-                          width: 200.0, // Ancho de la imagen
-                          height: 200.0, // Alto de la imagen
-                        ),
-                      ],
-                    ),
-                  ))
+                      ),
+                    ))
               ],
             ),
             Padding(
-              padding: const EdgeInsets.all(30.0),
+              padding: const EdgeInsets.only(top: 50.0, right: 30.0, left: 30.0),
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.end,
                 children: [
                   TextField(
                     controller: _emailController,
                     decoration:
-                        const InputDecoration(labelText: 'Usuario o Correo'),
+                        const InputDecoration(labelText: 'Correo institucional'),
                   ),
                   const SizedBox(height: 16.0),
                   TextField(
@@ -65,29 +70,42 @@ class LoginScreen extends StatelessWidget {
                     onPressed: () {
                       // Implementa la lógica para restablecer la contraseña
                     },
-                    child: const Text('¿Olvidó su contraseña?'),
+                    child: const Text(
+                      '¿Olvidó su contraseña?',
+                      style: TextStyle(fontSize: 16.0,),
+                    ),
                   ),
+                  const SizedBox(height: 16.0),
                   Consumer<LoginProvider>(
                     builder: (context, authProvider, child) {
-                      return ElevatedButton(
-                        onPressed: () async {
-                          try {
-                            await authProvider.login(
-                              _emailController.text,
-                              _passwordController.text,
-                            );
-                            // Redirige al usuario a la pantalla de perfil en lugar de la pantalla principal
-                            /*Navigator.pushReplacement(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => ProfileScreen()),
-                    );*/
-                          } catch (e) {
-                            // Manejo de errores de inicio de sesión
-                            print(e);
-                          }
-                        },
-                        child: const Text('Iniciar Sesión'),
+                      return SizedBox(
+                        width: MediaQuery.of(context).size.width,
+                        child: ElevatedButton(
+                          onPressed: () async {
+                            try {
+                              await authProvider.login(
+                                _emailController.text,
+                                _passwordController.text,
+                              );
+                              // Redirige al usuario a la pantalla de perfil en lugar de la pantalla principal
+                              /*Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => ProfileScreen()),
+                                          );*/
+                            } catch (e) {
+                              // Manejo de errores de inicio de sesión
+                              print(e);
+                            }
+                          },
+                          child: const Padding(
+                            padding: EdgeInsets.symmetric(vertical: 12.0),
+                            child: Text(
+                              'Iniciar Sesión',
+                              style: TextStyle(fontSize: 16.0,),
+                            ),
+                          ),
+                        ),
                       );
                     },
                   ),
@@ -96,7 +114,12 @@ class LoginScreen extends StatelessWidget {
                     onPressed: () {
                       // Implementa la navegación a la pantalla de registro
                     },
-                    child: const Text('¿No tiene cuenta? Registrarse gratis'),
+                    child: const Text(
+                      '¿No tiene cuenta? Registrarse gratis',
+                      style: TextStyle(
+                                fontSize: 16.0,
+                              ),
+                      ),
                   ),
                 ],
               ),
