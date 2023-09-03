233b2c87d7b08b4a38c82faaec1c2e0c85b3ba8e
Javier Revelo <shavi.sprint@gmail.com>
2023-09-03 00:59:41 -0500

    cambio calase persona y provider

--- lib/dtos/person_model.dart
+++ lib/dtos/person_model.dart
@@ -3,11 +3,19 @@ class Person {
   final String faculty;
   final String email;
   final String password;
+    int? edad;
+  String? descripcion;
+  String? genero;
+  String? imagen;
 
   Person({
     required this.name,
     required this.faculty,
     required this.email,
     required this.password,
+    this.edad,
+    this.descripcion,
+    this.genero,
+    this.imagen
   });
 }
--- lib/providers/person_provider.dart
+++ lib/providers/person_provider.dart
@@ -1,10 +1,16 @@
+import 'dart:collection';
+
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:proyecto/dtos/person_model.dart';
 
 class PersonProvider extends ChangeNotifier {
   final CollectionReference _personCollection =
       FirebaseFirestore.instance.collection('persona');
 
+  List<Person> _listaUsuarios = [];
+  UnmodifiableListView<Person> get usuariosGetter =>
+      UnmodifiableListView(_listaUsuarios);
   Future<void> addPerson({
     required String nombre,
     required String facultad,
@@ -23,5 +29,27 @@ class PersonProvider extends ChangeNotifier {
       print('Error al agregar la persona: $e');
       throw e;
     }
+  }
+
+  Future<void> _initEventsList() async {
+    final querySnapShot =
+        await FirebaseFirestore.instance.collection('events').get();
+
+    _listaUsuarios = querySnapShot.docs.map((doc) {
+      final data = doc.data();
+      print('////////////////// HOLA ////////////////');
+      return Person(
+          email: data['correo'],
+          faculty: data['facultad'],
+          name: data['nombre'],
+          password: data['clave'],
+          descripcion: "",
+          edad: 0,
+          genero: "",
+          imagen: "");
+    }).toList();
+
+    print(_listaUsuarios);
+    notifyListeners();
   }
 }
